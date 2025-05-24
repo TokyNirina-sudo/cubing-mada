fe090a5879327d983dcff59ff0de0d11cc00296b
TokyNirina-sudo <toky9353@gmail.com>
2025-05-24 06:08:15 +0300

    Initial commit

---
+++ components/classement/icon-categorie.tsx
@@ -0,0 +1,26 @@
+import { HoverCard, HoverCardTrigger } from "../ui/hover-card";
+import Image from "next/image";
+import { Button } from "../ui/button";
+import { HoverCardContent } from "@radix-ui/react-hover-card";
+export default function IconCategorie({
+  icon,
+  title,
+}: {
+  icon: string;
+  title: string;
+}) {
+  return (
+    <div className="item-center flex flex-col">
+      <HoverCard>
+        <HoverCardTrigger asChild>
+          <Button variant="outline">
+            <Image src={icon} alt="icon de categorie" width={14} height={14} />
+          </Button>
+        </HoverCardTrigger>
+        <HoverCardContent>
+          <span className=" text-sm font-semibold">{title}</span>
+        </HoverCardContent>
+      </HoverCard>
+    </div>
+  );
+}
